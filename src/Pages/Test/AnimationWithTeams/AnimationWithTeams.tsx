b208509897f7bab44b7aac119aa047546391bb7b
Sebastian <sebastianporttka@gmail.com>
2024-11-15 19:18:35 +0100

    test

--- src/Pages/Test/AnimationWithTeams/AnimationWithTeams.tsx
+++ src/Pages/Test/AnimationWithTeams/AnimationWithTeams.tsx
@@ -39,16 +39,16 @@ export const ChooseYourTeam = () => {
 
 	return (
 		<div
-			className={`${styles.animationWidthTeams} w-full min-h-[100svh] bg-white flex  overflow-x-hidden`}
+			className={`${styles.animationWidthTeams} w-full min-h-[100svh] bg-white flex flex-col overflow-x-hidden`}
 		>
 			<div className='w-[100%]  flex flex-col justify-start items-center gap-8'>
-				<div className='w-[100%] flex flex-col  py-16 gap-4 bg-red-700'>
-					<h1 className='text-xl text-white text-center font-Formula1-Bold'>
+				<div className='w-[100%] flex flex-col  py-16 gap-6 bg-red-700'>
+					<h1 className='text-md text-white text-center font-Formula1-Regular'>
 						Welcome in
 					</h1>
 					<h1 className='text-4xl text-center text-white'>F1 App</h1>
-					<div className='w-[90%] mx-auto text-center'>
-						<p className=' text-gray-300'>
+					<div className='w-[90%] mx-auto text-center animate-fadeUp'>
+						<p className=' text-white text-md font-Formula1-Regular'>
 							Developed out of a passion for Formula 1 and a love for creating
 							with React, this application was built as a hobby project. It is
 							not intended for commercial use.
@@ -61,7 +61,7 @@ export const ChooseYourTeam = () => {
 						Choose your favorite team and cheer in the colors of your favorite
 						squad!
 					</p>
-					<fieldset className='border-t-2 border-r-2 w-full border-red-700 rounded-tr-lg flex flex-col gap-2 pr-4 '>
+					<fieldset className='border-t-2 border-r-2 w-full border-red-700 rounded-tr-lg flex flex-col gap-2 pr-4 shadow-lg'>
 						<legend className='mx-4 px-2 font-semibold text-md text-gray-400 font-Formula1-Bold'>
 							Teams
 						</legend>
@@ -87,6 +87,7 @@ export const ChooseYourTeam = () => {
 					</fieldset>
 				</div>
 			</div>
+			<div className='w-full min-h-[4svh] bg-red-700'></div>
 		</div>
 	);
 };
