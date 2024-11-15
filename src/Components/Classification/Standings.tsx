955d9e2aa034d195544f8e2a66c1560d9a7063ef
Sebastian <sebastianporttka@gmail.com>
2024-11-15 07:17:15 +0100

    some changes

--- src/Components/Classification/Standings.tsx
+++ src/Components/Classification/Standings.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 
 import { CarAvatar } from "../Cars/CarAvatar";
 import { TeamLogo } from "../TeamLogos/TeamLogo";
@@ -6,6 +6,8 @@ import { TeamLogo } from "../TeamLogos/TeamLogo";
 import "./standings.css";
 import { DriverStandingCard } from "./Drivers/DriverStandingsCard/DriverStandingCard";
 import { useQuery } from "@tanstack/react-query";
+import { useAppContext } from "../Context/AppContext";
+import { AnimatedHeader } from "../../Pages/Test/AnimatedHeader";
 
 export type Driver = {
 	driverId: string;
@@ -57,10 +59,10 @@ const StandingPageHeader = () => {
 	);
 };
 
-export const DriverStandings: FC<DriverStandingsProps> = ({
-	driverResults,
-}) => {
+export const DriverStandings = () => {
 	const [expandedItemId, setExpandedItemId] = useState<number | undefined>();
+	const [driverInfo, setDriverInfo] = useState<StandingsTable | undefined>();
+	const { driverStandings, isLoading, error } = useAppContext();
 
 	const handleExpand = (index: number) => {
 		setExpandedItemId((prev) => {
@@ -72,81 +74,119 @@ export const DriverStandings: FC<DriverStandingsProps> = ({
 		});
 	};
 
+	useEffect(() => {
+		if (driverStandings?.MRData?.StandingsTable) {
+			setDriverInfo(driverStandings.MRData.StandingsTable);
+		}
+	}, [driverStandings]);
+
+	if (error) {
+		return (
+			<div>
+				<p>{error.message}</p>
+			</div>
+		);
+	}
+
+	if (isLoading) {
+		return (
+			<div>
+				<p>Ładowanie...</p>
+			</div>
+		);
+	}
+
+	const driverResults = driverInfo?.StandingsLists[0]?.DriverStandings;
+
 	return (
 		<>
-			{driverResults.map((result, index) => (
-				<DriverStandingCard
-					key={index}
-					result={result}
-					index={index}
-					expanded={expandedItemId === index}
-					expand={() => handleExpand(index)}
-				/>
-			))}
+			{!driverResults ? (
+				<AnimatedHeader />
+			) : (
+				driverResults.map((result, index) => (
+					<DriverStandingCard
+						key={index}
+						result={result}
+						index={index}
+						expanded={expandedItemId === index}
+						expand={() => handleExpand(index)}
+					/>
+				))
+			)}
 		</>
 	);
 };
 
-export const ConstructorStandings: FC<ConstructorStandingsProps> = ({
-	constructorResults,
-}) => {
+export const ConstructorStandings = () => {
+	const { constructorStandings, isLoading, error } = useAppContext();
+	const [constructorData, setConstructorData] = useState<
+		StandingsTable | undefined
+	>();
+
+	useEffect(() => {
+		if (constructorStandings?.MRData?.StandingsTable) {
+			setConstructorData(constructorStandings?.MRData?.StandingsTable);
+		}
+	});
+
+	if (error) {
+		return (
+			<div>
+				<p>{error.message}</p>
+			</div>
+		);
+	}
+
+	if (isLoading) {
+		return (
+			<div>
+				<p>Ładowanie...</p>
+			</div>
+		);
+	}
+
+	const constructorResults =
+		constructorData?.StandingsLists[0].ConstructorStandings;
+
 	return (
 		<>
-			{constructorResults.map((result) => (
-				<div key={result.Constructor?.constructorId}>
-					<div className='standings-card constructor-cards'>
-						<div className='standing-cards-wrapper'>
-							<div className='standings-position'>
-								<h2>{result.position}</h2>
-							</div>
-							<div className='constructor-standings-name'>
-								<h3>{result.Constructor?.name}</h3>
-							</div>
-							{/*  */}
-							<div className='team-standings-image'>
-								<TeamLogo Constructor={result.Constructor?.name} />
-							</div>
-							<div>
-								<div className='team-standings-points'>
-									<h4>{result.points}</h4>
-									<p>PTS</p>
+			{!constructorResults ? (
+				<AnimatedHeader />
+			) : (
+				constructorResults.map((result) => (
+					<div key={result.Constructor?.constructorId}>
+						<div className='standings-card constructor-cards'>
+							<div className='standing-cards-wrapper'>
+								<div className='standings-position'>
+									<h2>{result.position}</h2>
+								</div>
+								<div className='constructor-standings-name'>
+									<h3>{result.Constructor?.name}</h3>
+								</div>
+								{/*  */}
+								<div className='team-standings-image'>
+									<TeamLogo Constructor={result.Constructor?.name} />
+								</div>
+								<div>
+									<div className='team-standings-points'>
+										<h4>{result.points}</h4>
+										<p>PTS</p>
+									</div>
 								</div>
 							</div>
-						</div>
-						<div className='standings-car-animation'>
-							<CarAvatar Constructor={result.Constructor?.name} />
+							<div className='standings-car-animation'>
+								<CarAvatar Constructor={result.Constructor?.name} />
+							</div>
 						</div>
 					</div>
-				</div>
-			))}
+				))
+			)}
 		</>
 	);
 };
 
 export const StandingsResults = () => {
 	const [selectResult, setSelectResult] = useState<string>("driverstandings");
-
-	const { data, error, isLoading } = useQuery<{
-		MRData: { StandingsTable: StandingsTable };
-	}>({
-		queryKey: [selectResult],
-		queryFn: () =>
-			fetch(`/api/f1/current/${selectResult}.json`).then(
-				(res) => res.json()
-			),
-	});
-
-	const driverResults =
-		data?.MRData.StandingsTable?.StandingsLists[0].DriverStandings || [];
-	const constructorResults =
-		data?.MRData.StandingsTable?.StandingsLists[0].ConstructorStandings || [];
-
-	if (error)
-		return (
-			<div>
-				<p>error</p>
-			</div>
-		);
 
 	return (
 		<section className='app-wrapper standingsTable'>
@@ -166,17 +206,13 @@ export const StandingsResults = () => {
 				</button>
 			</div>
 
-			{isLoading ? (
-				<p>Loading....</p>
-			) : (
-				<div className='standings'>
-					{selectResult === "driverstandings" ? (
-						<DriverStandings driverResults={driverResults} />
-					) : (
-						<ConstructorStandings constructorResults={constructorResults} />
-					)}
-				</div>
-			)}
+			<div className='standings'>
+				{selectResult === "driverstandings" ? (
+					<DriverStandings />
+				) : (
+					<ConstructorStandings />
+				)}
+			</div>
 		</section>
 	);
 };
