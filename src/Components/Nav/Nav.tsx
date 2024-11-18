107d09468f0097d51d1c166ea4733c4345ee967c
Sebastian <sebastianporttka@gmail.com>
2024-11-18 02:00:09 +0100

    menu customize

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -2,32 +2,35 @@ import { Link } from "react-router-dom";
 
 export const Nav = () => {
 	return (
-		<div className='nav'>
+		<div className='w-full flex justify-evenly items-center fixed bottom-0 bg-white min-h-[9svh] z-50 border-t-[1px] border-black shadow-xl'>
 			<Link to='/'>
-				<div className='nav-item'>
+				<div className='transition-all flex flex-col items-center gap-1 hover:scale-105 '>
 					<img
 						src='https://cdn-icons-png.flaticon.com/512/1275/1275364.png'
 						alt=''
+						className="w-[30px]"
 					/>
-					<p>Drivers</p>
+					<p className="font-Formula1-Regular text-sm ">Drivers</p>
 				</div>
 			</Link>
 			<Link to='/racing'>
-				<div className='nav-item'>
+				<div className='transition-all flex flex-col items-center gap-1 hover:scale-105'>
 					<img
 						src='https://www.transparentpng.com/thumb/racing-flag/f1-flag-race-racing-icon-png-10.png'
 						alt=''
+						className="w-[30px]"
 					/>
-					<p>Racing</p>
+					<p className="font-Formula1-Regular text-sm ">Racing</p>
 				</div>
 			</Link>
 			<Link to='/standings'>
-				<div className='nav-item'>
+				<div className='transition-all flex flex-col items-center gap-1 hover:scale-105'>
 					<img
 						src='https://clipart-library.com/image_gallery/46370.jpg'
 						alt=''
+						className="w-[30px]"
 					/>
-					<p>Standings</p>
+					<p className="font-Formula1-Regular text-sm">Standings</p>
 				</div>
 			</Link>
 		</div>
