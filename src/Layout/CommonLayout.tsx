b7f58430581aabcb4e075bc341736757bf189b4a
Sebastian <sebastianporttka@gmail.com>
2024-11-18 02:37:23 +0100

    mobile secure

--- src/Layout/CommonLayout.tsx
+++ src/Layout/CommonLayout.tsx
@@ -7,30 +7,35 @@ import { Nav } from "../Components/Nav/Nav";
 import { ChooseYourTeam } from "../Pages/Test/AnimationWithTeams/AnimationWithTeams";
 
 export const CommonLayout = () => {
-  const { isLoading } = useAppContext();
-  const [showTeamSelection, setShowTeamSelection] = useState(true);
+	const { isLoading } = useAppContext();
+	const [showTeamSelection, setShowTeamSelection] = useState(true);
 
-  const handleTeamSelection = () => {
-    setShowTeamSelection(false);
-  };
+	const handleTeamSelection = () => {
+		setShowTeamSelection(false);
+	};
 
-  return (
-    <>
-      {isLoading ? (
-        <AnimatedHeader />
-      ) : (
-        <>
-          {showTeamSelection ? (
-            <ChooseYourTeam onTeamSelect={handleTeamSelection} />
-          ) : (
-            <>
-              <Nav />
-              <AppHeader />
-              <Outlet />
-            </>
-          )}
-        </>
-      )}
-    </>
-  );
+	return (
+		<>
+			{isLoading ? (
+				<AnimatedHeader />
+			) : (
+				<>
+					{showTeamSelection ? (
+						<ChooseYourTeam onTeamSelect={handleTeamSelection} />
+					) : (
+						<>
+							<div className="flex flex-col sm:hidden">
+								<Nav />
+								<AppHeader />
+								<Outlet />
+							</div>
+              <div className="hidden w-full min-h-[100vh] bg-dynamic sm:flex justify-center items-center">
+                      <h3>This app is only for mobile now ! </h3>
+              </div>
+						</>
+					)}
+				</>
+			)}
+		</>
+	);
 };
